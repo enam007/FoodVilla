029e0fdcbf92760b73bbfd12c4354a231d503530
Md Aamir Enam <aamireee007@gmail.com>
2023-11-07 22:07:55 +0530

    resolving cors error

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.0734262&lng=72.626571&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.0734262&lng=72.626571&restaurantId=";
 export const API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0734262&lng=72.626571&is-seo-homepage-enabled=true";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0734262&lng=72.626571&is-seo-homepage-enabled=true";
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const restaurantList = [
