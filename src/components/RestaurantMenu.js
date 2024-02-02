133698ec545f38a4265cff8d71277c80dfdc9f34
Md Aamir Enam <aamireee007@gmail.com>
2024-02-02 22:36:25 +0530

    adding cart logic

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,31 +1,61 @@
 import { useState } from "react";
 import { IMG_CDN_URL } from "../config.js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItem, removeItem } from "../Utils/cartSlice.js";
 
 const RestaurantMenu = ({ itemCards }) => {
-  const [showMenu, setShowMenu] = useState(false);
+  console.log(itemCards);
+  console.log(itemCards.length);
+  const [btnClicked, setBtnClicked] = useState(
+    new Array(itemCards.length).fill(false)
+  );
+  const cartItems = useSelector((store) => store.cart.items);
+  const [counter, setCounter] = useState(new Array(itemCards.length).fill(0));
+  console.log(counter);
 
   const dispatch = useDispatch();
 
-  const handleAddItems = (item) => {
+  const handleAddItems = (item, index) => {
     dispatch(addItem(item));
+    const newCounter = [...counter];
+    newCounter[index] = newCounter[index] + 1;
+    setCounter(newCounter);
   };
-  const handleRemoveItems = (item) => {
+  const handleRemoveItems = (item, index) => {
     dispatch(removeItem(item.card.info.id));
+    if (counter[index] == 1) {
+      const tempArr = [...btnClicked];
+      tempArr[index] = !tempArr[index];
+      setBtnClicked(tempArr);
+    }
+    if (counter[index] > 0) {
+      const newCounter = [...counter];
+      newCounter[index] = newCounter[index] - 1;
+      setCounter(newCounter);
+    }
+  };
+
+  const handleAddButton = (item, index) => {
+    dispatch(addItem(item));
+    const newCounter = [...counter];
+    newCounter[index] = newCounter[index] + 1;
+    setCounter(newCounter);
+    const tempArr = [...btnClicked];
+    tempArr[index] = !tempArr[index];
+    setBtnClicked(tempArr);
   };
 
   return (
     <>
       <div className="">
         <div className="mx-auto p-6 m-4 w-3/4">
-          {itemCards.map((item) => {
-            console.log("item", item);
+          {itemCards.map((item, index) => {
+            //console.log("item", item);
             return (
               <div className="flex justify-between p-2 m-4 h-36 border-b-2 pb-2 transform transition-transform hover:scale-105">
                 <div className="p-2 m-2 w-2/3 h-24">
                   <div>{item.card.info.name}</div>
-                  <div>{`₹ ${item.card.info.price / 100}`}</div>
+                  <div>{`₹ ${item?.card?.info?.price / 100}`}</div>
                   <div className="text-xs text-balance">
                     {item.card.info.description}
                   </div>
@@ -37,19 +67,29 @@ const RestaurantMenu = ({ itemCards }) => {
                     src={IMG_CDN_URL + item.card.info.imageId}
                   />
                   <div className="flex shadow-md hover:bg-slate-100 active:bg-slate-200 justify-around border-solid border-slate-100 rounded-md border-2 mt-1 pt-1 items-center">
+                    {btnClicked[index] && (
+                      <button
+                        className="w-1/3"
+                        onClick={() => handleRemoveItems(item, index)}
+                      >
+                        -
+                      </button>
+                    )}
                     <button
                       className="w-1/3"
-                      onClick={() => handleRemoveItems(item)}
+                      onClick={() => handleAddButton(item, index)}
                     >
-                      -
+                      {!btnClicked[index] ? "Add" : counter[index]}
+                      {console.log(counter[index])}
                     </button>
-                    <span className="w-1/3">Add</span>
-                    <button
-                      className="w-1/3"
-                      onClick={() => handleAddItems(item)}
-                    >
-                      +
-                    </button>
+                    {btnClicked[index] && (
+                      <button
+                        className="w-1/3"
+                        onClick={() => handleAddItems(item, index)}
+                      >
+                        +
+                      </button>
+                    )}
                   </div>
                 </div>
               </div>
